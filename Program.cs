ae061d27302235b530b48238360e0c47d64dd4c6
user <user@WIN-H2QPH3A41C1>
2024-07-11 11:57:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+
+                char again = 'y';
+                while (again == 'y')
+                {
+                    double total;
+                    double a;
+                    double b;
+                    char oper;
+
+                    Console.WriteLine("enter the first number and press enter");
+                    a = Convert.ToDouble(Console.ReadLine());
+
+
+                    Console.WriteLine("enter a math operation + - / * and press enter");
+                    oper = Convert.ToChar(Console.ReadLine());
+
+
+                    Console.WriteLine("enter the second number and press enter");
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                    if (oper == '+')
+                    {
+                        total = a + b;
+                        Console.WriteLine("Result: " + total);
+                    }
+                    else if (oper == '-')
+                    {
+
+                        total = a - b;
+                        Console.WriteLine("Result: " + total);
+
+                    }
+                    else if (oper == '*')
+                    {
+                        total = a * b;
+                        Console.WriteLine("Result: " + total);
+                    }
+
+                    else if (oper == '/')
+                    {
+                        if (b != 0)
+                        {
+                            total = a / b;
+                            Console.WriteLine(total);
+                            Console.WriteLine("Result: " + total);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You cannot divide by 0!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("unknown operator");
+                    }
+
+
+                    {
+                        Console.WriteLine("click to continue (y/n)");
+                        again = Convert.ToChar(Console.ReadLine());
+                    }
+                }
+
+
+
+            }
+
+
+
+        }
+    }
+}
+    
+
